9ed8a8541bc481478323e7beee469a44eddeed13
Jim <JLRennie@gmail.com>
2018-05-12 11:31:36 -0600

    Added basic database stuff

---
+++ src/main/java/io/angularattack/loweredexpectations/rankit/entities/RankedItem.java
@@ -0,0 +1,34 @@
+package io.angularattack.loweredexpectations.rankit.entities;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.Accessors;
+import org.hibernate.annotations.Type;
+
+import javax.persistence.*;
+import java.util.UUID;
+
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode(of = "id")
+@Accessors(chain = true)
+@Entity
+@Table(name = "RANKED_ITEM")
+public class RankedItem {
+
+    @Id
+    @Type(type = "uuid-char")
+    @Column(name = "ID")
+    private UUID id;
+
+    @Column(name = "NAME")
+    private String name;
+
+    @PrePersist
+    public void prePersist() {
+        id = UUID.randomUUID();
+    }
+}
---
+++ src/main/java/io/angularattack/loweredexpectations/rankit/repositories/RankedGroupRepository.java
@@ -0,0 +1,12 @@
+package io.angularattack.loweredexpectations.rankit.repositories;
+
+import io.angularattack.loweredexpectations.rankit.entities.RankedGroup;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface RankedGroupRepository extends JpaRepository<RankedGroup, UUID> {
+
+}
