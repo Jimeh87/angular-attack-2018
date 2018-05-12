8df230a197fe0b1b01faad10e4d6b5e5cb6b775a
Jim <JLRennie@gmail.com>
2018-05-12 14:24:32 -0600

    Added swagger

--- src/main/java/io/angularattack/loweredexpectations/rankit/controllers/HelloWorldController.java
+++ src/main/java/io/angularattack/loweredexpectations/rankit/controllers/HelloWorldController.java
@@ -5,9 +5,11 @@ import io.angularattack.loweredexpectations.rankit.api.HelloDto;
 import io.angularattack.loweredexpectations.rankit.services.HelloWorldService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/v1/hello-world")
 public class HelloWorldController {
 
     @Autowired
