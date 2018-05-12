e6be53065afa829ca03b15791fa3081e7e416233
Dalin Seivewright <dalin.seivewright@gmail.com>
2018-05-11 19:23:22 -0600

    Initial Commit

---
+++ src/main/java/io/angularattack/loweredexpectations/chili/ChiliApplication.java
@@ -0,0 +1,12 @@
+package io.angularattack.loweredexpectations.chili;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChiliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChiliApplication.class, args);
+	}
+}
---
+++ src/test/java/io/angularattack/loweredexpectations/ChiliApplicationTests.java
@@ -0,0 +1,16 @@
+package io.angularattack.loweredexpectations.chili;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ChiliApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
