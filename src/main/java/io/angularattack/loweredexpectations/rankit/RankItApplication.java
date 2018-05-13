98a67bb90a7ce61c53d0ac0a2aa385a85aafd07b
Jim <JLRennie@gmail.com>
2018-05-13 17:32:48 -0600

    Comment out mock data

--- src/main/java/io/angularattack/loweredexpectations/rankit/RankItApplication.java
+++ src/main/java/io/angularattack/loweredexpectations/rankit/RankItApplication.java
@@ -26,68 +26,68 @@ public class RankItApplication {
 
     @PostConstruct
     public void postConstruct() {
-        printIt(rankedGroupRepository.save(new RankedGroup()
-                .setName("Favorite Chili")
-                .setShortCode(RankedGroupService.generateShortCode())
-                .addAllRankedItems(new RankedItem()
-                                .setName("Jim's 5 alarm chili")
-                                .setScore(START_SCORE),
-                        new RankedItem()
-                                .setName("Dalin's tomato sauce he calls chili")
-                                .setScore(START_SCORE),
-                        new RankedItem()
-                                .setName("Marc's second place chili")
-                                .setScore(START_SCORE),
-                        new RankedItem()
-                                .setName("Peng says he is chili")
-                                .setScore(START_SCORE))
-        ));
-
-        printIt(rankedGroupRepository.save(new RankedGroup()
-                .setName("Coolest guy on the team")
-                .setShortCode(RankedGroupService.generateShortCode())
-                .addAllRankedItems(new RankedItem()
-                                .setName("Jim")
-                                .setScore(START_SCORE),
-                        new RankedItem()
-                                .setName("Dalin")
-                                .setScore(START_SCORE),
-                        new RankedItem()
-                                .setName("Marc")
-                                .setScore(START_SCORE),
-                        new RankedItem()
-                                .setName("Peng")
-                                .setScore(START_SCORE))
-        ));
-
-        printIt(rankedGroupRepository.save(new RankedGroup()
-                .setName("Favorite Color")
-                .setShortCode(RankedGroupService.generateShortCode())
-                .addAllRankedItems(new RankedItem()
-                                .setName("Blue")
-                                .setScore(START_SCORE),
-                        new RankedItem()
-                                .setName("Red")
-                                .setScore(START_SCORE),
-                        new RankedItem()
-                                .setName("Green")
-                                .setScore(START_SCORE),
-                        new RankedItem()
-                                .setName("Yellow")
-                                .setScore(START_SCORE),
-                        new RankedItem()
-                                .setName("Pink")
-                                .setScore(START_SCORE),
-                        new RankedItem()
-                                .setName("Orange")
-                                .setScore(START_SCORE),
-                        new RankedItem()
-                                .setName("Grey")
-                                .setScore(START_SCORE),
-                        new RankedItem()
-                                .setName("Brown")
-                                .setScore(START_SCORE))
-        ));
+//        printIt(rankedGroupRepository.save(new RankedGroup()
+//                .setName("Favorite Chili")
+//                .setShortCode(RankedGroupService.generateShortCode())
+//                .addAllRankedItems(new RankedItem()
+//                                .setName("Jim's 5 alarm chili")
+//                                .setScore(START_SCORE),
+//                        new RankedItem()
+//                                .setName("Dalin's tomato sauce he calls chili")
+//                                .setScore(START_SCORE),
+//                        new RankedItem()
+//                                .setName("Marc's second place chili")
+//                                .setScore(START_SCORE),
+//                        new RankedItem()
+//                                .setName("Peng says he is chili")
+//                                .setScore(START_SCORE))
+//        ));
+//
+//        printIt(rankedGroupRepository.save(new RankedGroup()
+//                .setName("Coolest guy on the team")
+//                .setShortCode(RankedGroupService.generateShortCode())
+//                .addAllRankedItems(new RankedItem()
+//                                .setName("Jim")
+//                                .setScore(START_SCORE),
+//                        new RankedItem()
+//                                .setName("Dalin")
+//                                .setScore(START_SCORE),
+//                        new RankedItem()
+//                                .setName("Marc")
+//                                .setScore(START_SCORE),
+//                        new RankedItem()
+//                                .setName("Peng")
+//                                .setScore(START_SCORE))
+//        ));
+//
+//        printIt(rankedGroupRepository.save(new RankedGroup()
+//                .setName("Favorite Color")
+//                .setShortCode(RankedGroupService.generateShortCode())
+//                .addAllRankedItems(new RankedItem()
+//                                .setName("Blue")
+//                                .setScore(START_SCORE),
+//                        new RankedItem()
+//                                .setName("Red")
+//                                .setScore(START_SCORE),
+//                        new RankedItem()
+//                                .setName("Green")
+//                                .setScore(START_SCORE),
+//                        new RankedItem()
+//                                .setName("Yellow")
+//                                .setScore(START_SCORE),
+//                        new RankedItem()
+//                                .setName("Pink")
+//                                .setScore(START_SCORE),
+//                        new RankedItem()
+//                                .setName("Orange")
+//                                .setScore(START_SCORE),
+//                        new RankedItem()
+//                                .setName("Grey")
+//                                .setScore(START_SCORE),
+//                        new RankedItem()
+//                                .setName("Brown")
+//                                .setScore(START_SCORE))
+//        ));
     }
 
     private static void printIt(RankedGroup rankedGroup) {
